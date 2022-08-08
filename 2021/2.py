f42b4d1ae594c1ce3389bc0b16fea4e44c3711da
RogerGabeller-ml <georgebarrell@gmail.com>
2022-08-08 23:50:44 +0100

    Added input text

--- 2021/2.py
+++ 2021/2.py
@@ -1,6 +1,7 @@
 with open("2.txt") as f:
     inp = [i.split() for i in f.readlines()]
 p1 = lambda k: sum([int(j) for i, j in inp if i==k])
+
 def p2():
     a = d = 0
     for i, j in inp:
@@ -13,5 +14,6 @@ def p2():
             case "up":
                 a -= j
     return d
+
 print((p1("down") - p1("up"))*p1("forward"))
 print(p2()*p1("forward"))
